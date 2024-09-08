6dea5822dca8850ebb7c2275e39c9b3d45728710
Nash Bean <iBeanSoftware@gmail.com>
2024-09-08 10:44:15 -0400

    Update Universe.cpp
    spell correct

--- cpp-project/src/Exercises/Universe.cpp
+++ cpp-project/src/Exercises/Universe.cpp
@@ -2,10 +2,12 @@
 #include "Universe.h"
 // }
 
-int Universe::CountAllStars(vector<int>& galaxies) {
+int Universe::CountAllStars(vector<int>& galaxies) 
+{
     int total_stars = 0;
-    for(int stars : galaxies) {
-        total_stars = total_stars + stars; // fix me! Did. Sep 8, 2024 by NafhBean
+    for(int stars : galaxies) 
+    {
+        total_stars = total_stars + stars; // fix me! Did. Sep 8, 2024 by NashBean
     }
     return total_stars;
 }
